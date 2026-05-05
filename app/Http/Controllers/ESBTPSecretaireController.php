ed1b051e1f058d6741f4a4c66f472e29a77ad204
James10192 <djedjelipatrick@gmail.com>
2026-05-05 11:24:25 +0000

    Fix secretary password reset import

--- app/Http/Controllers/ESBTPSecretaireController.php
+++ app/Http/Controllers/ESBTPSecretaireController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ESBTPSecretaireController extends Controller
 {
